a26d3710448b67e482a8223ff774daec5193937b
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-07-04 19:54:16 +0530

    Added management commands for inserting test data

---
+++ core/management/commands/insert_test_users.py
@@ -0,0 +1,17 @@
+from .test_data_command import TestDataCommand
+from .constants import N_USERS
+from core import models
+
+
+class Command(TestDataCommand):
+    def insert_data(self):
+        for i in range(1, N_USERS+1, 1):
+            req = dict(
+                first_name=f'F{str(i).zfill(3)}',
+                last_name=f'L{str(i).zfill(3)}',
+                email=f'test{str(i).zfill(3)}@test.com',
+                password = 'testtest'
+            )
+            user = models.User(**req)
+            user.set_password(req['password'])
+            user.save()
